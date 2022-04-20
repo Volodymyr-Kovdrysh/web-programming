cb0c15c3c3dedf03044b2bba8d71b1cc553661b0
VVKo <v.v.kovdrysh@gmail.com>
2022-04-20 09:27:38 +0300

    create UserSearch

---
+++ src/GitApp/components/users/UserSearch.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const UserSearch = () => {
+    return (
+        <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+            <div>
+                <form>
+                    <div className="form-control">
+                        <div className="relative">
+                            <input type="text"
+                                   className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                                    placeholder="Пошук"
+                            />
+                            <button type="submit" className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg">
+                                Вперед
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <buttun className="btn btn-ghost btn-lg">
+                    Очистити
+                </buttun>
+            </div>
+        </div>
+    );
+};
+
+export default UserSearch;
