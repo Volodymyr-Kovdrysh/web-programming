09c87bca9b8519312b3cb439aa48b7ed48b55f16
VVKo <v.v.kovdrysh@gmail.com>
2022-04-23 23:53:16 +0300

    remove console.log

--- src/Auth/context/AuthContext.js
+++ src/Auth/context/AuthContext.js
@@ -24,7 +24,7 @@ export const AuthProvider = ({children}) => {
                 body: JSON.stringify(formData)
             })
         const data = await response.json()
-        console.log('data',data)
+
         dispatch({
             type: 'LOGIN',
             payload: data
